ff771fa6d3c396771e69266817e0247e7fd63a18
Daniel Kugel <mail4dk@gmail.com>
2014-08-02 23:02:17 +0300

    Added log4net source code. Added sample project.

---
+++ Log4Net-Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net_Playground
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            log.Info("Hello World!");
+        }
+    }
+}
